c8efbbe2ff975f7b8fc5aaa547e5f8eb985b5537
RavenWang <zjqx1991@163.com>
2022-06-14 13:35:31 +0800

    稀疏数组

---
+++ src/main/java/com/raven/alg/array/SparseArray.java
@@ -0,0 +1,80 @@
+package com.raven.alg.array;
+
+
+/**
+ * 稀疏数组
+ */
+public class SparseArray {
+
+    /**
+     * 二维数组转 -> 稀疏数组
+     *
+     * @param array 二维数组
+     * @return
+     */
+    public static int[][] getSparseArray(int[][] array) {
+        if (0 == array.length) {
+            System.out.println("二维数组不能为空！！！");
+        }
+
+        // 获取二维数组行 和 列
+        Integer row = array.length;
+        Integer list = array[0].length;
+        Integer num = 0;
+        for (Integer i = 0; i < row; i++) {
+            for (Integer j = 0; j < list; j++) {
+                if (array[i][j] > 0) {
+                    num++;
+                }
+            }
+        }
+
+        // 创建稀疏数组
+        int[][] sparseArray = new int[num + 1][3];
+        // 记录二维数组信息
+        sparseArray[0][0] = row;
+        sparseArray[0][1] = list;
+        sparseArray[0][2] = num;
+
+        // 保存有效数据
+        int length = 0;
+        for (Integer i = 0; i < row; i++) {
+            for (Integer j = 0; j < list; j++) {
+                int value = array[i][j];
+                if (value > 0) {
+                    length++;
+                    sparseArray[length][0] = i;
+                    sparseArray[length][1] = j;
+                    sparseArray[length][2] = value;
+                }
+            }
+        }
+        return sparseArray;
+    }
+
+
+    /**
+     * 稀疏数组 -> 二维数组转
+     *
+     * @param sparseArray
+     * @return
+     */
+    public static int[][] sparseArrayToArray(int[][] sparseArray) {
+        if (0 == sparseArray.length) {
+            System.out.println("稀疏数组不能为空！！！");
+        }
+        int row = sparseArray[0][0];
+        int list = sparseArray[0][1];
+        // 创建二维数组
+        int[][] array = new int[row][list];
+
+        // 保存有效数据
+        for (Integer i = 1; i < sparseArray.length; i++) {
+            int i1 = sparseArray[i][0];
+            int i2 = sparseArray[i][1];
+            int v = sparseArray[i][2];
+            array[i1][i2] = v;
+        }
+        return array;
+    }
+}
---
+++ src/main/java/com/raven/alg/array/SparseArrayDemo.java
@@ -0,0 +1,44 @@
+package com.raven.alg.array;
+
+
+
+public class SparseArrayDemo {
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+        array[1][1] = 1;
+        array[1][2] = 2;
+        array[2][2] = 2;
+        array[2][3] = 3;
+        array[3][2] = 2;
+        array[4][4] = 3;
+
+        for (Integer i = 0; i < 5; i++) {
+            System.out.println();
+            for (Integer j = 0; j < 5; j++) {
+                System.out.printf("%d \t", array[i][j]);
+            }
+        }
+
+        System.out.println(" 稀疏数组 ");
+        int[][] sparseArray = SparseArray.getSparseArray(array);
+
+        for (Integer i = 0; i < sparseArray.length; i++) {
+            System.out.println();
+            for (Integer j = 0; j < 3; j++) {
+                System.out.printf("%d \t", sparseArray[i][j]);
+            }
+        }
+
+
+        System.out.println(" 稀疏数组 -> 二维数组");
+
+        int[][] array2 = SparseArray.sparseArrayToArray(sparseArray);
+
+        for (Integer i = 0; i < array2.length; i++) {
+            System.out.println();
+            for (Integer j = 0; j < array2[0].length; j++) {
+                System.out.printf("%d \t", array[i][j]);
+            }
+        }
+    }
+}
