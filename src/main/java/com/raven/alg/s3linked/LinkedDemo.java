b83a4a4e9a13defc4d9c79c4e76226fba4953390
RavenWang <zjqx1991@163.com>
2022-06-20 15:37:51 +0800

    无效单向列表

--- src/main/java/com/raven/alg/s3linked/LinkedDemo.java
+++ src/main/java/com/raven/alg/s3linked/LinkedDemo.java
@@ -1,60 +1,42 @@
 package com.raven.alg.s3linked;
 
 
-import java.util.Arrays;
-
 public class LinkedDemo {
     public static void main(String[] args) {
-
-        SingleLinkedOrder singleLinked = new SingleLinkedOrder();
-
-        SingleLinkedOrder singleLinked1 = new SingleLinkedOrder(1, "第-1");
-        SingleLinkedOrder singleLinked2 = new SingleLinkedOrder(2, "第-2");
-        SingleLinkedOrder singleLinked3 = new SingleLinkedOrder(3, "第-3");
-        SingleLinkedOrder singleLinked4 = new SingleLinkedOrder(4, "第-4");
-
-
-        singleLinked.add(singleLinked3);
-        singleLinked.add(singleLinked2);
-        singleLinked.add(singleLinked1);
-        singleLinked.add(singleLinked4);
-
-        singleLinked.list();
-
-        System.out.println("===== 修改 ====");
-        SingleLinkedOrder singleLinked5 = new SingleLinkedOrder(4, "第-5");
-        singleLinked.edit(singleLinked5);
-        singleLinked.list();
-
-        System.out.println("===== 删除 ====");
-        singleLinked.delete(1);
-        singleLinked.list();
-
-        System.out.println("===== 删除 ====");
-        singleLinked.delete(4);
-        singleLinked.list();
-
-        System.out.println("===== 获取-4 ====" + singleLinked.getNode(4));
-        System.out.println("===== 获取-1 ====" + singleLinked.getNode(1));
-
+        singleLinked();
     }
 
     /**
      * 无序链表
      */
     private static void singleLinked() {
-        SingleLinked singleLinked = new SingleLinked();
+        SingleLinked singleLinked = new SingleLinked(0, "", "");
 
-        SingleLinked singleLinked1 = new SingleLinked(1);
-        SingleLinked singleLinked2 = new SingleLinked(2);
-        SingleLinked singleLinked3 = new SingleLinked(3);
-        SingleLinked singleLinked4 = new SingleLinked(4);
+        SingleLinked singleLinked1 = new SingleLinked(1, "影魔", "SF");
+        SingleLinked singleLinked2 = new SingleLinked(2, "狙击手", "火枪");
+        SingleLinked singleLinked3 = new SingleLinked(3, "露娜","月骑");
+        SingleLinked singleLinked4 = new SingleLinked(4, "卓尔游侠", "小黑");
 
-        singleLinked.add(singleLinked3);
-        singleLinked.add(singleLinked2);
-        singleLinked.add(singleLinked1);
-        singleLinked.add(singleLinked4);
+        singleLinked.add2(singleLinked3);
+        singleLinked.add2(singleLinked2);
+        singleLinked.add2(singleLinked1);
+        singleLinked.add2(singleLinked4);
 
         singleLinked.list();
+        System.out.println("删除数据=====");
+        singleLinked.delete(4);
+        singleLinked.delete(5);
+        singleLinked.list();
+
+        System.out.println("更新数据=====");
+        SingleLinked singleLinked5 = new SingleLinked(2, "新手第一英雄", "矮子");
+        singleLinked.update(singleLinked5);
+        singleLinked.list();
+
+
+        System.out.println("查询数据=====");
+        singleLinked.get(1);
+        singleLinked.get(3);
+        singleLinked.get(5);
     }
 }
--- src/main/java/com/raven/alg/s3linked/SingleLinked.java
+++ src/main/java/com/raven/alg/s3linked/SingleLinked.java
@@ -1,71 +1,184 @@
 package com.raven.alg.s3linked;
 
+import sun.nio.cs.FastCharsetProvider;
+
 /**
  * 无序单向链表
  */
 public class SingleLinked {
-    // 链表数据
-    Integer data;
-    // 链表数据
+    // 链表顺序
+    Integer sort;
+    // 节点数据
+    String name;
+    // 节点数据
+    String shortName;
+    // 链表头节点
     private SingleLinked head;
-    // 链表数据
+    // 链表下一个节点
     private SingleLinked next;
+
 
     public SingleLinked() {
     }
 
-    public SingleLinked(Integer data) {
-        this.data = data;
+    public SingleLinked(Integer sort, String name, String shortName) {
+        this.sort = sort;
+        this.name = name;
+        this.shortName = shortName;
     }
 
     /**
-     * 添加数据
+     * 新增节点到链表的最后
      */
     public void add(SingleLinked item) {
-        if (this.head == null) {
-            this.head = new SingleLinked();
+        if (null == this.head) {
+            this.head = new SingleLinked(0, "head", "head");
         }
-        SingleLinked node = this.getLastNode();
-        SingleLinked lastNext = node.next;
-        node.next = item;
-        item.next = lastNext;
-    }
-
-
-    /**
-     * 输出链表数据
-     */
-    public void list() {
+        // 获取列表的头节点
         SingleLinked head = this.head;
         while (null != head.next) {
-            SingleLinked temp = head.next;
-            System.out.printf("%s\t", temp);
+            head = head.next;
+        }
+        // 获取最后一个有效节点的next（null）
+        SingleLinked nextNull = head.next;
+        head.next = item;
+        item.next = nextNull;
+    }
+
+
+    /**
+     * 新增节点到链表的最后
+     */
+    public void add2(SingleLinked item) {
+        if (null == this.head) {
+            this.head = new SingleLinked(0, "head", "head");
+        }
+        // 获取列表的头节点
+        SingleLinked head = this.head;
+        while (true) {
+            if (null == head.next) {
+                break;
+            }
+            head = head.next;
+        }
+        // 获取最后一个有效节点的next（null）
+        SingleLinked nextNull = head.next;
+        head.next = item;
+        item.next = nextNull;
+    }
+
+
+    /**
+     * 删除指定节点
+     *
+     * @param sort
+     */
+    public void delete(Integer sort) {
+        SingleLinked head = this.head;
+        Boolean check = false;
+        if (null == head || null == head.next) {
+            System.out.println("列表为空！！！");
+            return;
+        }
+        while (true) {
+            // 链表末尾
+            if (null == head.next) {
+                break;
+            }
+            // 找到数据，返回找到数据的前一个节点（head）
+            if (head.next.sort == sort) {
+                check = true;
+                break;
+            }
+            head = head.next;
+        }
+        if (check) {
+            head.next = head.next.next;
+        } else {
+            System.out.println(sort + " 数据不存在！！！");
         }
     }
 
     /**
-     * 获取最后一个节点
+     * 修改列表数据
      */
-    private SingleLinked getLastNode() {
-        SingleLinked lastNode = this.head;
-        while (null != lastNode.next) {
-            lastNode = lastNode.next;
+    public void update(SingleLinked item) {
+        SingleLinked head = this.head;
+        Boolean check = false;
+        if (null == head || null == head.next) {
+            System.out.println("列表为空！！！");
+            return;
         }
-        System.out.println("最后一个节点：" + lastNode);
-        return lastNode;
+        while (true) {
+            // 没有找到
+            if (null == head) {
+                break;
+            }
+            // 找到了
+            if (head.sort == item.sort) {
+                check = true;
+                break;
+            }
+            head = head.next;
+        }
+
+        if (check) {
+            head.name = item.name;
+            head.shortName = item.shortName;
+        } else {
+            System.out.println("没有找到该数据，无法修改！！！");
+        }
     }
 
+
     /**
-     * 链表为空
+     * 获取数据
      */
-    private Boolean isEmpty() {
-        return true;
+    public void get(Integer sort) {
+        SingleLinked head = this.head;
+        Boolean check = false;
+        if (null == head || null == head.next) {
+            System.out.println("列表为空！！！");
+            return;
+        }
+        while (true) {
+            // 没有找到
+            if (null == head) {
+                break;
+            }
+            // 找到了
+            if (head.sort == sort) {
+                check = true;
+                break;
+            }
+            head = head.next;
+        }
+
+        if (check) {
+            System.out.println(sort + "数据为：" + head);
+        } else {
+            System.out.println("无法找到" + sort + "！！！");
+        }
+    }
+
+
+    /**
+     * 输出列表
+     */
+    public void list() {
+        if (null == this.head || null == this.head.next) {
+            System.out.println("列表为空！！！");
+        }
+        // 获取列表的头节点
+        SingleLinked head = this.head;
+        while (null != head.next) {
+            System.out.println(head.next);
+            head = head.next;
+        }
     }
 
     @Override
     public String toString() {
-        return "SingleLinked{" +
-                "data=" + data +
-                '}';
+        return "[ sort=" + sort + ", name='" + name + ", shortName=" + shortName + " ]";
     }
 }
